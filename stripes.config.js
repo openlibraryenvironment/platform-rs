2447cb8c89b10bf9a42b97902f3db124409cb434
Ian Ibbotson <ianibbo@googlemail.com>
2019-01-15 13:56:28 +0000

    initial checkin of platform

---
+++ stripes.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  okapi: { 'url':'http://localhost:9130', 'tenant':'diku' },
+  config: {
+    // autoLogin: { username: 'diku_admin', password: 'admin' }
+    // logCategories: 'core,redux,connect,connect-fetch,substitute,path,mpath,mquery,action,event,perm,interface,xhr'
+    // logPrefix: 'stripes'
+    // logTimestamp: false
+    // showPerms: false
+    // showHomeLink: false
+    // listInvisiblePerms: false
+    // disableAuth: false
+    hasAllPerms: true
+  },
+  modules: {
+    '@folio/users': {},
+  },
+  branding: {
+    logo: {
+      src: './tenant-assets/opentown-libraries-logo.png',
+      alt: 'Opentown Libraries',
+    },
+    favicon: {
+      src: './tenant-assets/opentown-libraries-favicon.png',
+    },
+  },
+};
