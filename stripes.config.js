cc51ec8cb70e8cdb11102ca08bc3f96ea889cd7d
Mike Taylor <mike@indexdata.com>
2020-01-28 19:31:24 +0000

    Mike's modified version of ReShare platform

--- stripes.config.js
+++ stripes.config.js
@@ -43,3 +43,31 @@ module.exports = {
     },
   },
 };
+
+
+module.exports.config.showDevInfo = true;
+// module.exports.config.logCategories = 'substitute';
+if (false) {
+  module.exports.config.platformName = 'Mike ReShare';
+  module.exports.okapi =
+  // { 'url':'https://okapi-reshare.apps.k-int.com', 'tenant':'reshare' }
+  // { 'url':'http://demo.reshare-dev.indexdata.com:9130', 'tenant':'reshare' } // reshare_admin/admin
+  { 'url':'http://reshare.reshare-dev.indexdata.com:9130', 'tenant':'millersville' } // millersville_admin/admin1350
+  // { 'url':'http://localhost:9130', 'tenant':'diku' }
+  ;
+} else {
+  module.exports.config.welcomeMessage = 'ui-courses.front.welcome',
+  module.exports.config.platformName = 'Mike CR';
+  module.exports.modules['@folio/courses'] = {};
+  module.exports.modules['@folio/inventory'] = {};
+  'directory,request,supply,rs,box,consortia,scan,shipping,si,unbox'
+    .split(',')
+    .forEach(name => {
+      delete module.exports.modules[`@folio/${name}`];
+    });
+  module.exports.okapi =
+     { 'url':'https://okapi-flo.folio-dev.indexdata.com', 'tenant':'sim_daisy' }
+  // { 'url':'https://folio-snapshot-okapi.aws.indexdata.com', 'tenant':'diku' }
+  // { 'url':'http://shared-index.reshare-dev.indexdata.com:9130', 'tenant':'diku' }
+  ;
+}
