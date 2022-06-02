fa376e910d3179454c52b24f864c5cc78886c1c4
Jason Skomorowski <jason@indexdata.com>
2022-06-02 17:08:47 -0400

    e2e: support connecting to multiple tenants and drop localforage dep

--- cypress/support/login.js
+++ cypress/support/login.js
@@ -1,5 +1,3 @@
-import localforage from 'localforage';
-
 import Button from '@folio/stripes-testing/interactors/button';
 import Dropdown from '@folio/stripes-testing/interactors/dropdown';
 import TextField from '@folio/stripes-testing/interactors/text-field';
@@ -10,22 +8,32 @@ Cypress.Commands.add('login', (username, password, visitPath = { path: '/', wait
   // out, rather than using the UI, in accordance with the Best
   // Practices guidance at
   // https://docs.cypress.io/guides/references/best-practices.html#Organizing-Tests-Logging-In-Controlling-State
-  localforage.removeItem('okapiSess');
+  // localforage.removeItem('okapiSess');
 
-  cy.visit(visitPath.path);
+  // That other approach only worked for the base url and added an extra dep
+  cy.visit(visitPath.path, {
+    onBeforeLoad(win) {
+      win.localStorage.clear();
+      win.indexedDB.deleteDatabase('localforage');
+    },
+  });
 
-  // Todo: find the way to wrap interactor to cy chainable object
-  cy.do([
-    TextField('Username').fillIn(username),
-    TextField('Password').fillIn(password),
-    Button('Log in').click()
-  ]);
+  try {
+  cy.get(TextField('Username')).then(() => {
+    // Todo: find the way to wrap interactor to cy chainable object
+    cy.do([
+      TextField('Username').fillIn(username),
+      TextField('Password').fillIn(password),
+      Button('Log in').click()
+    ]);
 
-  // TODO: find the way how customize waiter timeout in case of interactors(cy.wrap may be)
-  // https://stackoverflow.com/questions/57464806/set-timeout-for-cypress-expect-assertion
-  // https://docs.cypress.io/api/commands/wrap#Requirements
+    // TODO: find the way how customize waiter timeout in case of interactors(cy.wrap may be)
+    // https://stackoverflow.com/questions/57464806/set-timeout-for-cypress-expect-assertion
+    // https://docs.cypress.io/api/commands/wrap#Requirements
 
-  visitPath.waiter();
+    visitPath.waiter();
+  });
+} catch (e) {}
 
   // There seems to be a race condition here: sometimes there is
   // re-render that happens so quickly that following actions like
