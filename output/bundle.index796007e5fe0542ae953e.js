ba38978a22a431a8c71b75a0cf94dd181d704198
David Igbinedion <david.igbinedion@yahoo.com>
2025-04-11 11:10:45 +0100

    Revert "test new checkout and check in versions"
    This reverts commit 3e9551c1f977ac8d5d8450df77b034aa0a1991f8.

--- output/bundle.index796007e5fe0542ae953e.js
+++
@@ -1,3 +0,0 @@
-"use strict";(self.webpackChunk_folio_platform_complete=self.webpackChunk_folio_platform_complete||[]).push([[57],{},o=>{var l=p=>o(o.s=p);o.O(0,[15],()=>l(98529));var s=o.O()}]);
-
-//# sourceMappingURL=bundle.index796007e5fe0542ae953e.js.map
